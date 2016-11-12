b28fa3297f1b1ea40de833fcbd887671352fe54e
Jeff Kumar <jeffk@vmware.com>
2016-11-11 20:11:11 -0500

    updates

--- my-bazaar/src/App.js
+++ my-bazaar/src/App.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
+
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>Welcome to VBazaar</h2>
+        <nav className="navbar navbar-default">
+            <div className="container-fluid">
+                <div className="navbar-header">
+                    <a className="navbar-brand" href="#">
+                        vBazzar
+                    </a>
+                </div>
+            </div>
+        </nav>
+        <div className="container">
+          <div className="row">
+              <div className="jumbotron" style={{marginTop: "10%"}} >
+                  <h4>Select Your Location:</h4>
+                  <div>
+                      <a className="button button-primary button-vbz" href="#">Atlanta</a>
+                      <a className="button button-primary button-vbz" href="#">San Francisco</a>
+                      <a className="button button-primary button-vbz" href="#">Banglore</a>
+                  </div>
+              </div>
+          </div>
         </div>
-
       </div>
     );
   }
